bf3f195756a5e1bd57f40ebf5c7fbdf42b7d9402
pietronicolettisenacti18 <110640021+pietronicolettisenacti18@users.noreply.github.com>
2023-02-22 22:13:05 -0300

    att 2202

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <main>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">HOME</a></li>
+                            <li><a class="dropdown-item" href="#">PRODUTOS</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">CONTATO</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <h1> Site de floricultura </h1>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="flor01.avif" class="d-block w-100" alt="flor">
+            </div>
+            <div class="carousel-item">
+                <img src="flor02.jpg" class="d-block w-100" alt="flor">
+            </div>
+            <div class="carousel-item">
+                <img src="flor03.webp" class="d-block w-100" alt="flor">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+
+                    <div class="card">
+                        <img src="flor01.avif " class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Arranjo de flor</h5>
+                            <p class="card-text">No nosso site voçê encontra os melhores arranjos de flores.</p>
+                            <a href="#" class="btn btn-primary">Compre já</a>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="col">
+
+                    <div class="card">
+                        <img src="flor02.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">flor</h5>
+                            <p class="card-text">somente no nosso site voçê encontra as melhores flores.</p>
+                            <a href="#" class="btn btn-primary">Compre já</a>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card">
+                        <img src="flor03.webp" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-success">flores</h5>
+                            <p class="card-text">somente no nosso site voçê encontra as melhores flores.</p>
+                            <a href="#" class="btn btn-outline-success btn-lg">Compre já</a>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <h2 class="display-6 text-center">CONTATO</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.328528678126!
+                    2d-47.331282685166165!3d-22.753186085090118!2m3!1f0!2f0!3f0!3m2!1i1024!
+                    2i768!4f13.1!3m3!1m2!1s0x94c8998174c896b9%3A0x739b7f99b3323574!
+                    2sSenac%20Americana!5e0!3m2!1spt-BR!2sbr!4v1677113348727!5m2!1spt-BR!2sbr"
+                     width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"
+                      referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+        </div>
+    </div>
+    </main>
+    <footer class="container bg-dark">
+        <div class="row">
+            <div class="col">
+                Floricultura
+            </div>
+        <div class="col">
+
+        </div>
+        </div>
+    </footer>
+</body>
+
+</html>
